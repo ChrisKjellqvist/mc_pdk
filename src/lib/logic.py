ff08f961c5a4a42d9431d665a87eeb27d46fb658
Chris Kjellqvist <chriskjellqvist@gmail.com>
2024-11-10 23:24:28 -0500

    something decent.

--- src/lib/logic.py
+++ src/lib/logic.py
@@ -3,14 +3,25 @@ import grammar as gr
 from src.lib.cell import InputPin, OutputPin
 
 # OR gate
-def gen_or(n_inputs):
+def gen_orn(n_inputs):
     assert 7 >= n_inputs >= 2
     c.Cell("OR%d" % n_inputs,
            c.COMBINATIONAL,
            0,
-           3 + 6 * (n_inputs-1),
+           4 * (2 * n_inputs-1),
            [InputPin(gr.alphabet_input[i]) for i in range(n_inputs)],
            [OutputPin("Y", "+".join(gr.alphabet_input[:n_inputs]))])
+    for i in range(1, n_inputs):
+        f = list(gr.alphabet_input[:n_inputs])
+        for j in range(i):
+            f[j] = "!" + f[j]
+        c.Cell(f"OR{n_inputs}_{i}",
+               c.COMBINATIONAL,
+               1,
+               4 * (2 * n_inputs-1),
+               [InputPin(gr.alphabet_input[i]) for i in range(n_inputs)],
+               [OutputPin("Y", "+".join(f))])
+
 
 # ANDD gate - AND "dense"
 # it's possible to have parallel wires with alternating repeaters but this adds
@@ -20,19 +31,23 @@ def gen_or(n_inputs):
 #     assert 7 >= n_inputs >= 2
 #     c.Cell("AND%d" % n_inputs, c.COMBINATIONAL, 2, 2 * n_inputs - 1)
 
-def gen_and(n_inputs):
+def gen_norn(n_inputs):
     """
-    AND for n_inputs > 2
+    NOR with inverted inputs (all inputs inverted equivalent to AND)
     :param n_inputs:
     :return:
     """
-    assert 7 >= n_inputs >= 3
-    c.Cell("AND%d" % n_inputs,
-           c.COMBINATIONAL,
-           2,
-           6 * (n_inputs + 2),
-           [InputPin(gr.alphabet_input[i]) for i in range(n_inputs)],
-              [OutputPin("Y", "*".join(gr.alphabet_input[:n_inputs]))])
+    assert 7 >= n_inputs >= 2
+    for i in range(n_inputs):
+        f = list(gr.alphabet_input[:n_inputs])
+        for j in range(i):
+            f[j] = "!" + f[j]
+        c.Cell(f"NOR{n_inputs}_{i}",
+               c.COMBINATIONAL,
+               2,
+               6 * (2 * n_inputs - 1),
+               [InputPin(gr.alphabet_input[i]) for i in range(n_inputs)],
+                  [OutputPin("Y", "*".join(f))])
 
 
 # ANDS gate - AND "sparse"
@@ -46,8 +61,10 @@ c.Cell("INV", c.COMBINATIONAL, 1, 9, [InputPin("A")], [OutputPin("Y", "!A")])
 c.Cell("NAND2", c.COMBINATIONAL, 2, 12, [InputPin("A"), InputPin("B")], [OutputPin("Y", "!(A*B)")])
 
 # OR2,3,4,5
-for i in range(4):
-    gen_or(i + 2)
+for i in range(5):
+    gen_orn(i + 2)
+    gen_norn(i + 2)
+
 
 c.Cell("MUX2", c.COMBINATIONAL, 4, 18, [InputPin("A"), InputPin("B"), InputPin("S")], [OutputPin("Y", "A*S + B*!S")])
 
--- test/test.v
+++ test/test.v
@@ -9,3 +9,29 @@ module top(
     assign c = a + b;
 
 endmodule
+
+/*
+ABC RESULTS:             NAND2 cells:       24
+ABC RESULTS:               OR2 cells:       15
+ABC RESULTS:              AND2 cells:       12
+Total: 51
+*/
+
+/*
+ABC RESULTS:               INV cells:        1
+ABC RESULTS:            NOR3_1 cells:        1
+ABC RESULTS:             NAND2 cells:       23
+ABC RESULTS:            NOR2_1 cells:        1
+ABC RESULTS:               OR2 cells:       15
+ABC RESULTS:              AND2 cells:       11
+Total: 52
+*/
+
+/*
+ABC RESULTS:             OR3_1 cells:        1
+ABC RESULTS:             OR2_0 cells:       14
+ABC RESULTS:             NAND2 cells:       22
+ABC RESULTS:               OR2 cells:        7
+ABC RESULTS:              AND2 cells:        7
+Total: 41
+*/
