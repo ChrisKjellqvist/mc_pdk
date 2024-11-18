7036d63068be5858eb984c08813800e929c55aae
Chris Kjellqvist <chriskjellqvist@gmail.com>
2024-11-18 00:04:20 -0500

    some scripts

--- src/physical/techlibgen.py
+++ src/physical/techlibgen.py
@@ -16,6 +16,12 @@ setDesignMode -process 500
 source mc_pdk.tcl
 set DESIGN_NAME {top_name}
 set SYN_PATH ./syn_out
-""")
 
-    with open("")
+source mc_pdk.tcl
+
+set_db max_cpus_per_server 8
+set_db init_hdl_search_path ../../src
+# if you have child modules, you need to add the paths of the other .lib files
+set_db library [list $slow_lib]
+set_db interconnect_mode wireload
+""")
