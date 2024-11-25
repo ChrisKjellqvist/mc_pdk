5de1f8fa3433aa2de0043fe97000c4b13fc41366
Chris Kjellqvist <chriskjellqvist@gmail.com>
2024-11-25 12:38:17 -0500

    obviously some problems but it generates some sort of placement without any crazy errors...

--- src/global_constants.py
+++ src/global_constants.py
@@ -1,15 +1,20 @@
 manufacturing_grid_size = 0.005
 pitch = manufacturing_grid_size * 40
 
-wire_width = manufacturing_grid_size * 8
-placement_grid_size = wire_width * 10
-# offset should put wire in teh middle of the placement grid
-wire_offset = placement_grid_size / 2 - wire_width / 2
-wire_spacing = placement_grid_size
 
 def to_n_decimals(f: float, n: int) -> str:
     a = str(f)
     if '.' in a:
         return a[:min(len(a)-1, n+1+a.index('.'))]
 
+
+def to_n_decimals_flt(f: float, n: int) -> float:
+    return float(to_n_decimals(f, n))
+
+
+wire_width = manufacturing_grid_size * 8
+placement_grid_size = to_n_decimals_flt(wire_width * 10, 4)
+# offset should put wire in teh middle of the placement grid
+wire_offset = to_n_decimals_flt(placement_grid_size / 2 - wire_width / 2, 4)
+wire_spacing = to_n_decimals_flt(placement_grid_size, 4)
 
